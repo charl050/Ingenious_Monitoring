b9745d65f8d651191b3ab224c1a6befe4667fc48
charl050 <55154508+charl050@users.noreply.github.com>
2020-05-04 14:22:56 +0200

    Add files via upload

--- routes.py
+++ routes.py
@@ -1,19 +1,17 @@
 from flask import Flask, render_template, flash, request, url_for, redirect, Markup
 from flask_bootstrap import Bootstrap
-from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
 import lib
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 app.config.from_object(__name__)
-app.config['SECRET_KEY'] = 'SjdnUends821Jsdlkvxh391ksdODnejdDw'
-
 sqlite_file = 'useful_data.sqlite'
 
+# The fonction for the home page
 @app.route('/',  methods=['GET', 'POST'])
 def index():
 
-
+    # Default values
     start_date = "2019-02-19"
     end_date= "2020-01-10"
     interval = "month"
@@ -26,23 +24,26 @@ def index():
 
     if request.method == 'POST':
 
+        # If the user send a post request, get the inputs
         start_date = request.values.get('date_from')
         end_date = request.values.get('date_to')
         interval = request.values.get('scale')
 
-        try:
+        try: # Try to prevent attack from the user
             total, success, fail, warning = lib.chart_details(sqlite_file, (start_date, end_date), "percentage")
             chart_conf = lib.chart_global(sqlite_file, start_date, end_date, interval)
             donut_courses = lib.chart_courses(sqlite_file, (start_date, end_date), "pourcentage")
         except:
             pass
 
+        # Return values to the template
         return render_template('index.html', total = total, success = success, fail = fail, warning = warning, chart_global = chart_conf, donut_courses = donut_courses, start_date_keep=start_date, end_date_keep=end_date, scale_keep=interval)
 
     else:
       return render_template('index.html', total = total, success = success, fail = fail, warning = warning, chart_global = chart_conf, donut_courses = donut_courses, start_date_keep=start_date, end_date_keep=end_date, scale_keep=interval)
 
 
+# Fonction for the course page
 @app.route('/courses',  methods=['GET', 'POST'])
 def courses():
     start_date = "2019-02-19"
@@ -65,14 +66,14 @@ def courses():
         end_date = request.values.get('date_to')
         interval = request.values.get('scale')
 
-        #try:
-        total, success, fail, warning = lib.chart_details_courses(sqlite_file, (start_date, end_date), "percentage", course)
-        chart_conf = lib.chart_global_courses(sqlite_file, start_date, end_date, interval, course)
-        donut_courses = lib.chart_courses(sqlite_file, (start_date, end_date), "pourcentage")
-        donut_tasks = lib.chart_task(sqlite_file, (start_date, end_date), "pourcentage", course)
+        try:
+            total, success, fail, warning = lib.chart_details_courses(sqlite_file, (start_date, end_date), "percentage", course)
+            chart_conf = lib.chart_global_courses(sqlite_file, start_date, end_date, interval, course)
+            donut_courses = lib.chart_courses(sqlite_file, (start_date, end_date), "pourcentage")
+            donut_tasks = lib.chart_task(sqlite_file, (start_date, end_date), "pourcentage", course)
 
-        #except Exception as e:
-        #    print(e)
+        except:
+            pass
 
         return render_template('courses.html', total = total, success = success, fail = fail, warning = warning, chart_global = chart_conf, donut_tasks = donut_tasks, donut_courses = donut_courses, start_date_keep=start_date, end_date_keep=end_date, scale_keep=interval, course_keep = course)
 
