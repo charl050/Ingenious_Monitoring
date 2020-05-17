7200345ca4856f83a56c2728310b32be762717b0
charl050 <55154508+charl050@users.noreply.github.com>
2020-05-17 09:36:00 +0200

    Add files via upload

--- routes.py
+++ routes.py
@@ -6,14 +6,15 @@ app = Flask(__name__)
 bootstrap = Bootstrap(app)
 app.config.from_object(__name__)
 sqlite_file = 'useful_data.sqlite'
+sqlite_file_1 = 'inginious.sqlite'
 
-# The fonction for the home page
+# The function for the home page
 @app.route('/',  methods=['GET', 'POST'])
 def index():
 
     # Default values
     start_date = "2019-02-19"
-    end_date= "2020-01-10"
+    end_date = "2020-01-10"
     interval = "month"
 
 
@@ -29,27 +30,34 @@ def index():
         end_date = request.values.get('date_to')
         interval = request.values.get('scale')
 
-        try: # Try to prevent attack from the user
+        try: # Try to prevent bad inputs from the user
             total, success, fail, warning = lib.chart_details(sqlite_file, (start_date, end_date), "percentage")
             chart_conf = lib.chart_global(sqlite_file, start_date, end_date, interval)
             donut_courses = lib.chart_courses(sqlite_file, (start_date, end_date), "pourcentage")
-        except:
-            pass
 
-        # Return values to the template
-        return render_template('index.html', total = total, success = success, fail = fail, warning = warning, chart_global = chart_conf, donut_courses = donut_courses, start_date_keep=start_date, end_date_keep=end_date, scale_keep=interval)
+            return render_template('index.html', total = total, success = success, fail = fail, warning = warning, chart_global = chart_conf, donut_courses = donut_courses, start_date_keep=start_date, end_date_keep=end_date, scale_keep=interval, error = None)
+
+
+        except:
+            # Return error alert
+            return render_template('index.html', total = total, success = success, fail = fail, warning = warning, chart_global = chart_conf, donut_courses = donut_courses, start_date_keep=start_date, end_date_keep=end_date, scale_keep=interval, error = True)
 
     else:
-      return render_template('index.html', total = total, success = success, fail = fail, warning = warning, chart_global = chart_conf, donut_courses = donut_courses, start_date_keep=start_date, end_date_keep=end_date, scale_keep=interval)
+      return render_template('index.html', total = total, success = success, fail = fail, warning = warning, chart_global = chart_conf, donut_courses = donut_courses, start_date_keep=start_date, end_date_keep=end_date, scale_keep=interval, error = None)
 
 
-# Fonction for the course page
-@app.route('/courses',  methods=['GET', 'POST'])
-def courses():
+# Function for the detailed page
+@app.route('/details',  methods=['GET', 'POST'])
+def details():
+
     start_date = "2019-02-19"
     end_date= "2020-01-10"
     interval = "month"
     course = 'LSINF1252'
+    task = 'All'
+    list = ['LSINF1101_PYTHON', 'LEPL1402', 'LSINF1252']
+    task_item = ['All'] + lib.tasks(sqlite_file_1, (start_date, end_date))[list.index(course)]
+    leng = len(task_item)
 
 
     total, success, fail, warning = lib.chart_details_courses(sqlite_file, (start_date, end_date), "percentage", course)
@@ -61,26 +69,38 @@ def courses():
     if request.method == 'POST':
 
         course = request.values.get('course')
+        task = request.values.get('task')
 
         start_date = request.values.get('date_from')
         end_date = request.values.get('date_to')
         interval = request.values.get('scale')
 
+
         try:
-            total, success, fail, warning = lib.chart_details_courses(sqlite_file, (start_date, end_date), "percentage", course)
-            chart_conf = lib.chart_global_courses(sqlite_file, start_date, end_date, interval, course)
-            donut_courses = lib.chart_courses(sqlite_file, (start_date, end_date), "pourcentage")
-            donut_tasks = lib.chart_task(sqlite_file, (start_date, end_date), "pourcentage", course)
+            if task != 'All':
+                task_item = ['All'] + lib.tasks(sqlite_file_1, (start_date, end_date))[list.index(course)]
+                leng = len(task_item)
+                total, success, fail, warning = lib.chart_details_task(sqlite_file_1, (start_date, end_date), "percentage",course, task)
+                chart_conf = lib.chart_global_task(sqlite_file, start_date, end_date, interval, task, course)
 
-        except:
-            pass
+            else:
+                task_item = ['All'] + lib.tasks(sqlite_file_1, (start_date, end_date))[list.index(course)]
+                leng = len(task_item)
+                total, success, fail, warning = lib.chart_details_courses(sqlite_file, (start_date, end_date), "percentage", course)
+                chart_conf = lib.chart_global_courses(sqlite_file, start_date, end_date, interval, course)
+                donut_courses = lib.chart_courses(sqlite_file, (start_date, end_date), "pourcentage")
+                donut_tasks = lib.chart_task(sqlite_file, (start_date, end_date), "pourcentage", course)
 
-        return render_template('courses.html', total = total, success = success, fail = fail, warning = warning, chart_global = chart_conf, donut_tasks = donut_tasks, donut_courses = donut_courses, start_date_keep=start_date, end_date_keep=end_date, scale_keep=interval, course_keep = course)
+            return render_template('details.html', total = total, success = success, fail = fail, warning = warning, chart_global = chart_conf, donut_tasks = donut_tasks, donut_courses = donut_courses, start_date_keep=start_date, end_date_keep=end_date, scale_keep=interval, course_keep = course, task_keep = task, len = leng, task_item = task_item, error = None)
+
+
+        except :
+            return render_template('details.html', total = total, success = success, fail = fail, warning = warning, chart_global = chart_conf, donut_tasks = donut_tasks, donut_courses = donut_courses, start_date_keep=start_date, end_date_keep=end_date, scale_keep=interval, course_keep = course, task_keep = task, len = leng, task_item = task_item, error = True)
+
+
 
     else:
-      return render_template('courses.html', total = total, success = success, fail = fail, warning = warning, chart_global = chart_conf, donut_tasks = donut_tasks, donut_courses = donut_courses, start_date_keep=start_date, end_date_keep=end_date, scale_keep=interval, course_keep = course)
-
-
+      return render_template('details.html', total = total, success = success, fail = fail, warning = warning, chart_global = chart_conf, donut_tasks = donut_tasks, donut_courses = donut_courses, start_date_keep=start_date, end_date_keep=end_date, scale_keep=interval, course_keep = course, task_keep = task, len = leng, task_item = task_item, error = None)
 
 
 app.run(debug=True, port = 80)
