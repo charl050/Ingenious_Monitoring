9c29ba87c4e303e8b5c253ce575bb5ad99741233
charl050 <55154508+charl050@users.noreply.github.com>
2020-05-17 19:20:35 +0200

    Add files via upload

--- create_database.py
+++ create_database.py
@@ -1,6 +1,8 @@
+
 # Use this function to create the database
 
 from lib import *
+from os import *
 
 def create_useful_database():
     """
@@ -8,27 +10,26 @@ def create_useful_database():
     """
     if not path.exists("useful_data.sqlite"):
         # date list
-        l = dates("data_inginious.sqlite")
+        l = dates("inginious.sqlite")
+        print(l)
         # create submissions table
-        d = results("data_inginious.sqlite",l)
+        d = results("inginious.sqlite",l)
         create_submissions_table("useful_data.sqlite", d)
         # create courses table
-        d = results_courses("data_inginious.sqlite",l)
+        d = results("inginious.sqlite",l)
         create_courses_table("useful_data.sqlite", d)
         #create LSINF1101-PYTHON table
-        d = LSINF1101_PYTHON_tasks("data_inginious.sqlite", l)
+        d = LSINF1101_PYTHON_tasks("inginious.sqlite", l)
         create_LSINF1101_PYTHON_table("useful_data.sqlite", d)
         #create LEPL1402 table
-        d = LEPL1402_tasks("data_inginious.sqlite", l)
+        d = LEPL1402_tasks("inginious.sqlite", l)
         create_LEPL1402_table("useful_data.sqlite", d)
         #create LSINF1252 table
-        d = LSINF1252_tasks("data_inginious.sqlite", l)
+        d = LSINF1252_tasks("inginious.sqlite", l)
         create_LSINF1252_table("useful_data.sqlite", d)
         print("useful_data.sqlite has been created well")
     else:
         print("useful_data.sqlite already exists")
-        
-if __name__ == 'main':
+
+if __name__ == '__main__':
   create_useful_database()
-  
-  
